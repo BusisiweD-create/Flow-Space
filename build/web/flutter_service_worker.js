6bcf952d0474685044b3d350b7969f2a9334c66d
Busisiwe Dhlamini <dhlaminibusisiwe30@gmail.com>
2025-12-09 10:54:04 +0200

    Fix API base URL to Render backend

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -48,14 +48,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "9e6573c4766b24beab1ab0df7c56af2e
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "d5cdd13c55a64e340775e45162950ebd",
+"flutter_bootstrap.js": "208ae9f9c332bb6ea7a5cc53b3a03008",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "a928df75dede85a30b12f9378013eb7f",
 "/": "a928df75dede85a30b12f9378013eb7f",
-"main.dart.js": "e5131ccc6998327e0d8ceffe474daf74",
+"main.dart.js": "fa91f57e28a98226af7aaa17831b89e7",
 "manifest.json": "e74b976acb596ce325daecad61bff38c",
 "version.json": "8d98e49e8fbfe0e15e2972419563e184"};
 // The application shell files that are downloaded before a service worker can
